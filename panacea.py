05fc9fc9aee59ad3c4e6334f41fdc5f6f0119829
grzeimann <grzeimann@gmail.com>
2016-12-14 08:46:30 -0600

    debugging

--- panacea.py
+++ panacea.py
@@ -393,7 +393,8 @@ def reduce_science(args):
                                  virusconfig=args.configdir)
                 sci2.load_fibers()
                 sci2.load_all_cal()
-                print(len(sci2.fibers),args.sci_df['Files'][ind].replace(amp, Amp_dict[amp][0]))
+                print(len(sci2.fibers),args.sci_df['Files'][ind].replace(amp, Amp_dict[amp][0]),
+                      args.sci_df['Output'][ind])
                 sci2.sky_subtraction()
                 outname = op.join(args.sci_df['Output'][ind],
                                   'S%s_%s_sci_%s.fits' %(
