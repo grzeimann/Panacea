e1ca096c8afbd855c7b18ed2300f203288ba47ee
grzeimann <grzeimann@gmail.com>
2016-12-20 16:28:00 -0600

    collapsed cubes, Co, files

--- panacea.py
+++ panacea.py
@@ -155,6 +155,7 @@ def make_cube_file(args, filename, ifucen, scale, side):
         data2 = F2[0].data
         a,b = data1.shape
         if a != len(ifucen[:,1]):
+            print("Length of IFUcen file not the same as Fe. Skipping Cube")
             return None
         data = np.vstack([data1,data2])
         x = np.arange(ifucen[:,1].min()-scale, ifucen[:,1].max()+scale, scale)
