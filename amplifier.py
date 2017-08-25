a681d7caf2f988a46edd149f7bcc206a712cd3ba
grzeimann <grzeimann@gmail.com>
2017-08-25 08:32:39 -0500

    divide by zero/nan issue??

--- amplifier.py
+++ amplifier.py
@@ -653,14 +653,15 @@ class Amplifier:
                                                    'pixelflat_cam%s_%s.fits' 
                                             %(self.specid, self.amp)))[0].data,
                                   dtype=float)
-            self.image[:] = np.where(pixelflat > 1e-8, self.image / pixelflat, 
+            self.image[:] = np.where(pixelflat > 1e-1, self.image / pixelflat, 
                                      0.0)
-            self.error[:] = np.where(pixelflat > 1e-8, self.error / pixelflat, 
-                                     0.0)
+            self.error[:] = np.where(pixelflat > 1e-1, self.error / pixelflat, 
+                                     -1.0)
+            self.mask = np.where(pixelflat > 1e-1, 0.0, -1.0)
             sel = np.isfinite(self.image)
             if (~sel).sum()>0:
                 self.image[~sel] = 0.0
-                self.error[~sel] = 0.0
+                self.error[~sel] = -1.0
 
     def subtract_background(self):
         if not self.image_prepped:
@@ -1263,7 +1264,8 @@ class Amplifier:
                                       satlevel=-1.0)
             cc.run(maxiter=self.cosmic_iterations)
             c = np.where(cc.mask == True)
-            self.mask = np.zeros(self.image.shape)
+            if self.mask is None:
+                self.mask = np.zeros(self.image.shape)
             for x, y in zip(c[0], c[1]):
                 self.mask[x][y] = -1.0
             self.error[c[0],c[1]] = -1.0
