e28da3230a620cff1a1bce443ccb0b20ceaff4af
grzeimann <grzeimann@gmail.com>
2018-06-18 08:21:45 -0500

    yup

--- combine_amp_reductions.py
+++ combine_amp_reductions.py
@@ -246,7 +246,6 @@ def main():
     R.flux = rect_spec[np.array(fibinds, dtype=int), :].sum(axis=0) / frac
     R.skyflux = rect_sky[np.array(fibinds, dtype=int), :].sum(axis=0) / frac
     R.fluxerror = noise * np.sqrt(len(fibinds)) / frac
-    R.spectrum = np.array([rect_wave, R.flux, R.skyflux, R.skyerror, frac])
     R.save(image_list=['image_name', 'error', 'ifupos', 'skypos', 'wave',
                        'oldspec', 'ftf', 'sky', 'skysub'],
            name_list=['image', 'error', 'ifupos', 'skypos', 'wave', 'oldspec',
