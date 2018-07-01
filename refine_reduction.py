85a4b711db1f775ac848c891dbdda1ad2718039a
grzeimann <grzeimann@gmail.com>
2018-07-01 11:10:33 -0500

    ready for action

--- refine_reduction.py
+++ refine_reduction.py
@@ -9,15 +9,19 @@ import argparse as ap
 import numpy as np
 import os.path as op
 
+from astropy.convolution import Gaussian2DKernel, interpolate_replace_nans
+from astropy.convolution import Gaussian1DKernel
 from astropy.io import fits
 from astropy.stats import SigmaClip, biweight_midvariance
 from fiber_utils import bspline_x0
 from input_utils import setup_logging
-from photutils import Background2D, BiweightLocationBackground
+from photutils import Background2D, SExtractorBackground, detect_sources
 from scipy.interpolate import interp1d
 from scipy.signal import savgol_filter
 from utils import biweight_location
 from wave_utils import get_new_wave
+from sklearn.gaussian_process.kernels import Matern, WhiteKernel, ConstantKernel
+from sklearn.gaussian_process import GaussianProcessRegressor
 
 
 def setup_my_parser(args=None):
@@ -44,6 +48,11 @@ def setup_my_parser(args=None):
                         type=str,
                         default='work/03946/hetdex/maverick/red1/reductions')
 
+    parser.add_argument("-op", "--outpath",
+                        help='''Outpath for adjusted reductions''',
+                        type=str,
+                        default='work/03946/hetdex/maverick/red1/reductions')
+
     parser.add_argument("-in", "--instrument",
                         help='''Instrument, e.g., virus''',
                         type=str, default='virus')
@@ -59,6 +68,7 @@ def setup_my_parser(args=None):
     parser.add_argument("-rc", "--recalculate_wavelength",
                         help='''recalculate_wavelength''',
                         action="count", default=0)
+
     args = parser.parse_args(args=args)
     args.log = setup_logging('great_code_ever')
 
@@ -84,6 +94,19 @@ def build_filename(rootdir, date, instrument, obs, expn, multiname):
                        'exp%02d' % expn, instrument,
                        multiname)
     return filename
+
+
+def set_multi_extensions(outpath, multiname, amps, nfibs, images=[], names=[]):
+    for i, amp in enumerate(amps):
+        fn = multiname + ('_%s.fits' % amp)
+        try:
+            F = fits.open(fn)
+        except:
+            continue
+        for name, image in zip(names, images):
+            F[name].data = np.array(image[(i*nfibs):((i+1)*nfibs)] * 1.,
+                                    dtype='float32')
+        F.writeto(op.join(outpath, op.basename(fn)), overwrite=True)
 
 
 def get_multi_extensions(multiname, amps):
@@ -126,9 +149,13 @@ def rectify(wave, spec, lims, fac=2.5):
         for i in np.arange(N):
             dw = np.diff(wave[i])
             dw = np.hstack([dw[0], dw])
-            I = interp1d(wave[i], spec[i] / dw, kind='quadratic',
-                         bounds_error=False, fill_value=-999.)
-            rect_spec[i, :] = I(rect_wave)
+            sel = spec[i] > 1e-3
+            if sel.sum() > 10:
+                I = interp1d(wave[i][sel], (spec[i] / dw)[sel], kind='quadratic',
+                             bounds_error=False, fill_value=-999.)
+                rect_spec[i, :] = I(rect_wave)
+            else:
+                rect_spec[i, :] = 0.0
     else:
         D = len(wave)
         rect_wave = np.linspace(lims[0], lims[1], int(D*fac))
@@ -141,21 +168,35 @@ def rectify(wave, spec, lims, fac=2.5):
     return rect_wave, rect_spec
 
 
-def fit_bspline(rect_wave, avg):
-    B, c = bspline_x0(rect_wave, nknots=wave.shape[1])
+def define_outliers(y, model, neigh=2):
+    thresh = 5. * np.sqrt(biweight_midvariance(y - model))
+    sel = np.where(np.abs(y - model) > thresh)[0]
+    for j in np.arange(1, neigh+1):
+        sel = np.union1d(sel, sel + 1)
+        sel = np.union1d(sel, sel - 1)
+    sel = sel[np.where((sel >= 0) + (sel < len(y)))[0]]
+    outlier = np.zeros(y.shape)
+    outlier[sel] = True
+    return np.array(outlier, dtype=bool)
+
+
+def fit_bspline(rect_wave, avg, knots=1032):
+    B, c = bspline_x0(rect_wave, nknots=knots)
     smooth = np.dot(c, np.linalg.lstsq(c[~avg.mask, :], avg[~avg.mask])[0])
     return np.ma.array(smooth, mask=avg.mask)
 
 
-def get_avg_spec(wave, spec, twi, lims):
+def get_avg_spec(wave, spec, twi, lims, mask=None):
+    if mask is None:
+        mask = np.ones((wave.shape[0],), dtype=bool)
     rect_wave, rect_spec = rectify(wave, spec, lims)
     rect_wave, rect_twi = rectify(wave, twi, lims)
     y = np.ma.array(rect_spec, mask=((rect_spec == 0.) + (rect_spec == -999.)))
     t = np.ma.array(rect_twi, mask=((rect_twi == 0.) + (rect_twi == -999.)))
     fac = np.ma.median(t, axis=1)[:, np.newaxis] / np.ma.median(t)
     norm = y / fac
-    avg = biweight_location(norm, axis=(0,))
-    smooth = fit_bspline(rect_wave, avg)
+    avg = biweight_location(norm[mask], axis=(0,))
+    smooth = fit_bspline(rect_wave, avg, knots=wave.shape[1])
     return rect_wave, rect_spec, y, norm, avg, smooth, fac
 
 
@@ -163,8 +204,8 @@ def fit_continuum(wv, sky, sncut=3., skip=1, fil_len=95, func=np.array):
     skym_s = 1. * sky
     sky_sm = savgol_filter(skym_s, fil_len, 1)
     allind = np.arange(len(wv), dtype=int)
+    mask = np.zeros(sky.shape, dtype=bool)
     for i in np.arange(5):
-        mad = np.median(np.abs(sky - sky_sm))
         mad = np.sqrt(biweight_midvariance(sky-sky_sm))
         outlier = func(sky - sky_sm) > sncut * mad
         sel = np.where(outlier)[0]
@@ -175,9 +216,12 @@ def fit_continuum(wv, sky, sncut=3., skip=1, fil_len=95, func=np.array):
         sel = sel[skip:-skip]
         good = np.setdiff1d(allind, sel)
         skym_s = 1.*sky
-        skym_s[sel] = np.interp(wv[sel], wv[good], sky_sm[good])
+        I = interp1d(wv[good], sky_sm[good], kind='linear', bounds_error=False,
+                     fill_value="extrapolate")
+        skym_s[sel] = I(wv[sel])
         sky_sm = savgol_filter(skym_s, fil_len, 1)
-    return sky_sm, sel
+    mask[sel] = True
+    return sky_sm, mask
 
 
 def longest_consecutive_mask(outliers):
@@ -192,47 +236,134 @@ def longest_consecutive_mask(outliers):
     return tot
 
 
-def build_ftf(norm, smooth, fac):
-    y = (norm - smooth) / smooth
-    y[y.mask] = 0.0
-    y.mask[y.data == 0.0] = True
-    sigma_clip = SigmaClip(sigma=3., iters=10)
-    bkg_estimator = BiweightLocationBackground()
-    bkg1 = Background2D(y.data[:224], (15, 251), filter_size=(1, 1),
-                        sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
-    bkg2 = Background2D(y.data[224:], (15, 251), filter_size=(1, 1),
-                        sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
-    background = np.vstack([bkg1.background, bkg2.background])
-    z = np.ma.median(y - background, axis=1)
-    x = np.arange(len(z))
-    cont, outliers = fit_continuum(x, z)
-    L = 2 * longest_consecutive_mask(outliers)
-    if not (L & 1):
-        L += 1
-    L = np.max([L, 15])
-    mask = np.zeros(y.data.shape, dtype=bool)
-    mask[outliers] = True
-    mask[y.data == 0.0] = True
-    bkg1 = Background2D(y.data[:224], (L, 51), filter_size=(1, 1),
-                        mask=mask[:224], sigma_clip=sigma_clip,
-                        bkg_estimator=bkg_estimator)
-    bkg2 = Background2D(y.data[224:], (L, 51), filter_size=(1, 1),
-                        mask=mask[224:], sigma_clip=sigma_clip,
-                        bkg_estimator=bkg_estimator)
-    background = np.vstack([bkg1.background, bkg2.background])
-    return fac + background, x, z, cont, outliers, bkg1
+def build_ftf(y, rect_wave, fac, bad, good, nfibs, knots=100):
+    mask = np.zeros((y.shape[0],))
+    mask[bad] = 1.
+    mask = np.array(mask, dtype=bool)
+    ftf = []
+    for i in np.arange(4):
+        xl = i * nfibs
+        xh = (i + 1) * nfibs
+        X = y[xl:xh] * 1.
+        X.mask[mask[xl:xh]] = True
+        avg_offset_spec = np.ma.median(X, axis=0)
+        smoothed = fit_bspline(rect_wave, avg_offset_spec, knots=knots)
+        ftf.append(smoothed + fac[xl:xh])
+    return np.vstack(ftf)
 
+
+def simple_flat_field(X, func=np.abs, fil_len=21, sncut=1.5, skip=2):
+    x = np.arange(X.shape[0])
+    cont = x * 0.
+    outlier = x * 0.
+    z = np.ma.median(X, axis=1)
+    for i in np.arange(4):
+        xl = i * 112
+        xh = (i + 1) * 112
+        cont[xl:xh], outlier[xl:xh] = fit_continuum(x[xl:xh], z[xl:xh],
+                                                    fil_len=fil_len, skip=skip,
+                                                    func=func, sncut=sncut)
+    return z - cont, cont
+
+
+def make_frame(xloc, yloc, data, scale=1.,
+               seeing_fac=1.5):
+    seeing = seeing_fac * scale
+    a = len(data)
+    x = np.arange(xloc.min()-scale,
+                  xloc.max()+1*scale, scale)
+    y = np.arange(yloc.min()-scale,
+                  yloc.max()+1*scale, scale)
+    xgrid, ygrid = np.meshgrid(x, y)
+    zimage = xgrid * 0.
+    d = np.zeros((a,)+xgrid.shape)
+    w = np.zeros((a,)+xgrid.shape)
+    for i in np.arange(len(x)):
+        for j in np.arange(len(y)):
+            d[:, j, i] = np.sqrt((xloc - xgrid[j, i])**2 +
+                                 (yloc - ygrid[j, i])**2)
+            w[:, j, i] = np.exp(-1./2.*(d[:, j, i]/seeing)**2)
+
+    sel = np.where((np.abs(data) > 1e-5) * np.isfinite(data))[0]
+    ws = w[sel, :, :].sum(axis=0)
+    zimage = ((data[sel, np.newaxis, np.newaxis] * w[sel]).sum(axis=0) /
+              ws * 1.9)
+    return xgrid, ygrid, zimage
+
+
+def get_sex_background(image, filt_size=21, cols=25):
+    sigma_clip = SigmaClip(sigma=3., iters=10)
+    bkg_estimator = SExtractorBackground()
+    bkg = Background2D(image, (filt_size, cols), filter_size=(1, 1),
+                       bkg_estimator=bkg_estimator, sigma_clip=sigma_clip,
+                       mask=image.mask)
+    return bkg.background
+
+
+def setup_GP():
+    kernel = (ConstantKernel() + Matern(length_scale=2, nu=3/2) +
+              WhiteKernel(noise_level=1.))
+    G = GaussianProcessRegressor(alpha=1e-10, copy_X_train=True, kernel=kernel,
+                                 n_restarts_optimizer=0, normalize_y=False,
+                                 optimizer='fmin_l_bfgs_b', random_state=None)
+    return G
+
+
+def fit_GP(wave, spec, mask):
+    G = setup_GP()
+    G.fit(wave[mask, np.newaxis], spec[mask])
+    return G.predict(wave[:, np.newaxis]), G
+
+
+def smooth_fiber(X, mask, nfibs):
+    z = np.ma.median(X, axis=1)
+    z.data[mask] = np.nan
+    z = np.array(z)
+    x = np.arange(len(z))
+    model = z * 0.
+    for i in np.arange(4):
+        xl = i * nfibs
+        xh = (i + 1) * nfibs
+        sel = np.isfinite(z[xl:xh])
+        G = setup_GP()
+        G.fit(x[xl:xh][sel, np.newaxis], z[xl:xh][sel])
+        model[xl:xh] = G.predict(x[xl:xh, np.newaxis])
+    return model
+
+
+def mask_sources(xgrid, ygrid, xpos, ypos, zimage, sncut=2.0):
+    sigma_clip = SigmaClip(sigma=3., iters=10)
+    bkg_estimator = SExtractorBackground()
+    bkg = Background2D(zimage, (25, 25), filter_size=(1, 1),
+                       sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+    threshold = bkg.background + (sncut * bkg.background_rms)
+    kernel = Gaussian2DKernel(2, x_size=5, y_size=5)
+    kernel.normalize()
+    segm = detect_sources(zimage, threshold, npixels=8, filter_kernel=kernel)
+    dist = np.sqrt((xgrid - xpos[:, np.newaxis, np.newaxis])**2 +
+                   (ygrid - ypos[:, np.newaxis, np.newaxis])**2)
+    fiberloc = np.argmin(dist, axis=0)
+    return np.unique(fiberloc[segm.array > 0])
 
 args = setup_my_parser(args=['-d', '20180624', '-o', '8', '-e', '1', '-m',
-                             'multi_321_026_035'])
+                             'multi_311_036_053'])
+
+if args.instrument == 'virus':
+    args.nfibs = 112
+if args.instrument == 'lrs2':
+    args.nfibs = 140
 
 for multi in args.multiname:
+    args.log.info('Grabbing info for %s' % multi)
     multipath = build_filename(args.rootdir, args.date, args.instrument,
                                args.observation, args.exposure_number, multi)
     xpos, ypos, spec, wave, twi, trace, ftf = get_multi_extensions(multipath,
                                                                    args.amps)
+    args.log.info('Getting average specrum for %s' % multi)
     returned_list = get_avg_spec(wave, spec, twi, args.lims)
     rect_wave, rect_spec, y, norm, avg, smooth, fac = returned_list
+    returned_list = get_avg_spec(wave, twi, twi, args.lims)
+    rect_wave, rect_twi, y_twi, norm_twi, avg_twi, smooth_twi, fac = returned_list
     if args.recalculate_wavelength:
         newwave = wave * 0.
         args.log.info('Working on the wavelength for side L')
@@ -241,16 +372,65 @@ for multi in args.multiname:
         args.log.info('Working on the wavelength for side R')
         newwave[224:] = get_new_wave(wave[224:], trace[224:], twi[224:],
                                      rect_wave, avg, smooth)
-        returned_list = get_avg_spec(newwave, spec, twi, args.lims)
+        wave0 = wave * 1.
+        wave = newwave * 1.
+        returned_list = get_avg_spec(wave, spec, twi, args.lims)
         rect_wave, rect_spec, y, norm, avg, smooth, fac = returned_list
-    ftf, x, z, cont, outliers, bkg = build_ftf(norm, smooth, fac)
-    new_norm = y / ftf
-    new_avg = biweight_location(new_norm, axis=(0,))
-    new_smooth = fit_bspline(rect_wave, new_avg)
-    new_ftf, x, z, cont, outliers, bkg = build_ftf(new_norm, new_smooth, ftf)
-    fits.PrimaryHDU(new_ftf.data).writeto('test.fits', overwrite=True)
-    fits.PrimaryHDU((y - new_ftf*new_smooth).data).writeto('test_res.fits',
-                                                           overwrite=True)
+        returned_list = get_avg_spec(wave, twi, twi, args.lims)
+        rect_wave, rect_twi, y_twi, norm_twi, avg_twi, smooth_twi, fac = returned_list
+    X = rect_spec / rect_twi * smooth_twi / smooth
+    flat_field, cont = simple_flat_field(X)
+    xgrid, ygrid, zimage = make_frame(xpos, ypos, flat_field)
+    mask = mask_sources(xgrid, ygrid, xpos, ypos, zimage)
+    good = np.setdiff1d(np.arange(X.shape[0], dtype=int), mask)
+    good_mask = np.zeros((X.shape[0],))
+    good_mask[good] = 1.
+    good_mask = np.array(good_mask, dtype=bool)
+    args.log.info('Building fiber to fiber for %s' % multi)
+    Y = X * 1.
+    faci = []
+    SM = X * 1.
+    for i in np.arange(4):
+        xl = i * args.nfibs
+        xh = (i + 1) * args.nfibs
+        returned_list = get_avg_spec(wave[xl:xh], spec[xl:xh], twi[xl:xh],
+                                     args.lims, mask=good_mask[xl:xh])
+        rect_wave, rs, y, norm, avg, smooth, fac = returned_list
+        faci.append(fac*1.)
+        Y[xl:xh] = (rs  -  smooth * faci[-1]) / smooth
+        SM[xl:xh] = smooth
+    fac = np.vstack(faci)
+    model = smooth_fiber(Y, mask, args.nfibs)[:, np.newaxis]
+    Z = (rect_spec - SM * (fac + model)) / SM
+    Z.mask[mask] = True
+    Z.mask[np.ma.abs(Z) > 0.25] = True
+    ftf = rect_spec * 0.
+    for i in np.arange(4):
+        xl = i * args.nfibs
+        xh = (i + 1) * args.nfibs
+        back = get_sex_background(Z[xl:xh], 11, 121)
+        ftf[xl:xh] = fac[xl:xh] + model[xl:xh] + back
+    ftf_new = wave * 0.
+    skysub_new = wave * 0.
+    sky_new = wave * 0.
+    for i in np.arange(wave.shape[0]):
+        dw = np.diff(wave[i])
+        dw = np.hstack([dw[0], dw])
+        I = interp1d(rect_wave, ftf[i], kind='quadratic',
+                     bounds_error=False, fill_value=-999.)
+        ftf_new[i] = I(wave[i]) * 1.
+        J = interp1d(rect_wave, ftf[i]*SM[i], kind='quadratic',
+                     bounds_error=False, fill_value=-999.)
+        skysub_new[i] = spec[i] - J(wave[i]) * dw
+        sky_new[i] = J(wave[i]) * dw
+
+    outpath = build_filename(args.outpath, args.date, args.instrument,
+                             args.observation, args.exposure_number, multi)
+    outpath = op.dirname(outpath)
+    set_multi_extensions(outpath, multipath, args.amps, args.nfibs,
+                         images=[ftf_new, sky_new, skysub_new],
+                         names=['fiber_to_fiber', 'sky_spectrum',
+                                'sky_subtracted'])
 
 
 def main():
