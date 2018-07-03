817f119a7ce436328906913f2bc76794822cd683
grzeimann <grzeimann@gmail.com>
2018-07-03 14:08:07 -0500

    faster?

--- wave_utils.py
+++ wave_utils.py
@@ -16,16 +16,14 @@ def eval_fit(x1, x2):
 
 def get_translation(wave, spec, cols, avg, xp, smooth, rect_wave,
                     maxmove=4.):
-
     dw = np.diff(wave)
     dw = np.hstack([dw[0], dw])
-
+    I = interp1d(wave, spec / dw, kind='quadratic',
+                 bounds_error=False, fill_value=-999.)
     def f(shifts):
         s = []
         for shift in shifts:
-            I = interp1d(wave+shift, spec / dw, kind='quadratic',
-                         bounds_error=False, fill_value=-999.)
-            rect_spec = I(rect_wave)
+            rect_spec = I(rect_wave-shift)
             ys = np.ma.array(rect_spec, mask=((rect_spec == 0.) +
                                               (rect_spec == -999.)))
             py1 = ys[~avg.mask][cols, np.newaxis]
@@ -55,7 +53,8 @@ def get_new_wave(wave, trace, spec, rect_wave, avg, smooth, maxmove=4.,
     NFIBS = wave.shape[0]
     shifts = []
     warray, xarray, yarray, oarray = ([], [], [], [])
-    for ind in np.arange(NFIBS):
+    inds = np.array(np.hstack([np.arange(1, NFIBS, 8), NFIBS-1]), dtype=int)
+    for ind in inds:
         for j, cols in enumerate(col_chunk):
             newwave[ind] = wave[ind] * 1.
             xp = x[cols]
@@ -73,13 +72,14 @@ def get_new_wave(wave, trace, spec, rect_wave, avg, smooth, maxmove=4.,
     x, y, w = [np.hstack(i) for i in [xarray, yarray, warray]]
     wi, sh = [np.array(v).reshape(NFIBS, nchunks) for v in [oarray, shifts]]
     newwave = wave * 1.
-    for ind in np.arange(NFIBS):
+    for ind in inds:
         x = np.interp(wi[ind], wave[ind], np.arange(wave.shape[1]))
         p0 = np.polyfit(x / (wave.shape[1] * 1.), wi[ind]+sh[ind], 3)
         newwave[ind] = np.polyval(p0, np.arange(wave.shape[1]) /
                                   (wave.shape[1] * 1.))
     for i in np.arange(newwave.shape[1]):
-        x = trace[:, i] / (newwave.shape[1] * 1.)
-        y = newwave[:, i] * 1.
-        newwave[:, i] = np.polyval(np.polyfit(x, y, 3), x)
+        x = trace[inds, i] / (newwave.shape[1] * 1.)
+        xfit = trace[:, i] / (newwave.shape[1] * 1.)
+        y = newwave[inds, i] * 1.
+        newwave[:, i] = np.polyval(np.polyfit(x, y, 3), xfit)
     return newwave
