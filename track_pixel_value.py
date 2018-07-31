afc1092425a22779af03a77900f2407ff1547275
grzeimann <grzeimann@gmail.com>
2018-07-31 13:45:59 -0500

    update

--- track_pixel_value.py
+++ track_pixel_value.py
@@ -45,7 +45,7 @@ def grab_info(itm, xran, yran):
     return y, st
 
 
-def Track_pixel_value(file_list, ifuslot, amp, args, date, yran=[0, 2064],
+def Track_pixel_value(file_list, ifuslot, amp, args, date, yran=[0, 1032],
                       xran=[2032, 2128]):
     # Create empty lists for the left edge jump, right edge jump, and structure
     big_array = np.zeros((len(file_list), (yran[1] - yran[0]),
