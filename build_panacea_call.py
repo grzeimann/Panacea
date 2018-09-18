ab6e9b0442e9acacc1ae2da31fe5395471c54d40
grzeimann <grzeimann@gmail.com>
2018-09-18 11:17:12 -0500

    update on use

--- build_panacea_call.py
+++ build_panacea_call.py
@@ -112,7 +112,6 @@ for _object in object_table:
     keystring = date+'_'+obsid
     date_tup = (int(date[:4]), int(date[4:6]), int(date[6:]))
     if DB < date_tup < DE:
-        print(filename[-8:-5])
         if fnmatch.fnmatch(objectname.lower(), args.target.lower()) and filename[-8:-5] == 'sci':
             if args.exposure_time is not None:
                 if exptime > args.exposure_time:    
