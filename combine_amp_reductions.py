1e71962bf9f760233a5969c836556c3778d8b5d9
grzeimann <grzeimann@gmail.com>
2018-09-24 10:00:39 -0500

    looking for stability

--- combine_amp_reductions.py
+++ combine_amp_reductions.py
@@ -466,8 +466,13 @@ def quick_exam(R, nwavebins, lims, side, args, name):
                                                           R.ftf),
                                             dtype='float64'), lims,
                                    fac=2.5)
+    sel = (rect_spec == -999.).sum(axis=1) < 1
+    rect_wave = rect_wave[sel]
+    rect_spec = rect_spec[sel, :]
     if args.emission:
         rect_spec1 = rect_spec * 1.
+        sky = biweight_location(rect_spec[sel, :], axis=(0, ))
+        rect_spec = rect_spec - sky[np.newaxis, :]
         rect_spec = convolve_spatially(R.ifux, R.ifuy, rect_spec, rect_wave,
                                        name, sig_wave=2.5*1.5)
         S = []
