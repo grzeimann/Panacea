0ff15a69e0cd3e7195fbfaabe8404a575a708a7b
grzeimann <grzeimann@gmail.com>
2018-10-15 17:19:17 -0500

    modifications

--- simple_detect_script.py
+++ simple_detect_script.py
@@ -161,9 +161,6 @@ t1 = time.time()
 cnt = 0
 cnt2 = 0
 for ifuslot in ifuslots:
-    if cnt2 == 5:
-        break
-    cnt2 += 1
     for amp in virus_amps:
         log.info('Starting on ifuslot, %s, and amp, %s' % (ifuslot, amp))
         twibase = twi_path % ('virus', 'virus', twi_obs, 'virus', ifuslot)
