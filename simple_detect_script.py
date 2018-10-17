a4a8d9f5f7bd914eca392a1e455ff326486e7e95
grzeimann <grzeimann@gmail.com>
2018-10-17 17:33:37 -0500

    quick

--- simple_detect_script.py
+++ simple_detect_script.py
@@ -120,9 +120,6 @@ def get_sciflat_field(flt_path, amp, array_wave, array_trace, common_wave,
     for filename in files:
         log.info('Working on sciflat %s' % filename)
         array_flt = base_reduction(filename) - masterbias
-        shift, error, diffphase = register_translation(default, array_flt, 100)
-        log.info(shift)
-        shifts.append(shift)
         x = np.arange(array_wave.shape[1])
         spectrum = array_trace * 0.
         for fiber in np.arange(array_wave.shape[0]):
@@ -143,12 +140,16 @@ def get_sciflat_field(flt_path, amp, array_wave, array_trace, common_wave,
         I = interp1d(nw1, ns1, kind='quadratic', fill_value='extrapolate')
         modelimage = I(bigW)
         flat = array_flt / modelimage
-        X = np.arange(array_flt.shape[1])
-        Y = np.arange(array_flt.shape[0])
+        listflat.append(flat)
+    for flat in listflat:
+        shift, error, diffphase = register_translation(listflat[0], flat, 100)
+        log.info(shift)
+        shifts.append(shift)
+        X = np.arange(flat.shape[1])
+        Y = np.arange(flat.shape[0])
         I = interp2d(X, Y, flat, kind='cubic', bounds_error=False,
                      fill_value=0.0)
-        #flat = I(X+shift[1], Y+shift[0])
-        listflat.append(flat)
+        flat = I(X-shift[1], Y-shift[0])
     flat = biweight_location(listflat, axis=(0,))
     flat[~np.isfinite(flat)] = 0.0
     flat[flat < 0.0] = 0.0
@@ -162,7 +163,7 @@ def get_sciflat_field(flt_path, amp, array_wave, array_trace, common_wave,
         array_flt = base_reduction(filename) - masterbias
         x = np.arange(array_wave.shape[1])
         spectrum = array_trace * 0.
-        nflat = J(X, Y+shift[0])
+        nflat = J(X+shift[1], Y+shift[0])
         for fiber in np.arange(array_wave.shape[0]):
             indl = np.floor(array_trace[fiber]).astype(int)
             indh = np.ceil(array_trace[fiber]).astype(int)
@@ -171,7 +172,7 @@ def get_sciflat_field(flt_path, amp, array_wave, array_trace, common_wave,
         I = interp1d(nw, ns, kind='quadratic', fill_value='extrapolate')
         modelimage = I(bigW)
         residual.append(array_flt - modelimage*nflat)
-    return flat, np.array(listflat)
+    return flat, np.array(residual)
 
 
 def get_masterbias(zro_path, amp):
