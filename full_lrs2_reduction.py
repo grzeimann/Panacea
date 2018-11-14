663cb490f5112e559f0e6f5af0e28ed8870c1d12
grzeimann <grzeimann@gmail.com>
2018-11-14 11:23:33 -0600

    making functional

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -523,7 +523,7 @@ def get_wavelength_from_arc(image, trace, brightline, lines, lims):
         cols = lines['col2'][i] + found_lines[:, i+1] - lines['col2'][i+1]
         for j, loci in enumerate(loc):
             dist = np.abs(loci - cols[j])
-            if dist < 5.:
+            if np.min(dist) < 5.:
                 found_lines[j, i] = loci[np.argmin(dist)]
         if (found_lines[:, i] > 0.).sum() < (0.5 * trace.shape[0]):
             found_lines[:, i] = 0.0
