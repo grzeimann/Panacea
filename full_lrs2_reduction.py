5343ea606de0469df1146acf834d83e77863a86a
grzeimann <grzeimann@gmail.com>
2018-11-16 10:45:08 -0600

    close

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -627,7 +627,7 @@ def get_wavelength_from_arc(image, trace, lines, side):
     m = (diff[1] - diff[0]) / (lines['col2'][-1] - lines['col2'][0])
     y = np.array(m * (lines['col2'] - lines['col2'][0]) +
                  diff[0] + lines['col2'])
-    # print(loc[fib], y, ph[fib])
+    print(loc[fib], y, ph[fib])
     for i, line in enumerate(lines):
         col = y[i]
         v = np.abs(col - loc[fib])
