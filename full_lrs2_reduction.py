79d8b6736ed49a20462ac8cd0f608b8dd20cd3f2
grzeimann <grzeimann@gmail.com>
2018-11-30 11:27:49 -0600

    note

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -52,12 +52,12 @@ parser.add_argument("-s", "--side",
 args = parser.parse_args(args=None)
 
 blueinfo = [['BL', 'uv', '503_056_7001', [3640., 4645.], ['LL', 'LU'],
-             [4350., 4375.], ['Hg_B', 'Cd-A_B', 'FeAr_R']],
+             [4350., 4375.], ['Hg_B', 'Cd-A_B', 'FeAr_R', 'Cd_B']],
             ['BR', 'orange', '503_056_7001',
              [4635., 6950.], ['RU', 'RL'], [6270., 6470.],
              ['Hg_B', 'Cd-A_B', 'FeAr_R']]]
 redinfo = [['RL', 'red', '502_066_7002', [6450., 8400.], ['LL', 'LU'],
-            [7225., 7425.], ['Hg_R', 'Cd-A_B', 'FeAr_R']],
+            [7225., 7425.], ['Hg_R', 'Cd-A_B', 'FeAr_R', 'Cd_B']],
            ['RR', 'farred', '502_066_7002',
             [8275., 10500.], ['RU', 'RL'], [9280., 9530.],
             ['Hg_R', 'Cd-A_B', 'FeAr_R']]]
