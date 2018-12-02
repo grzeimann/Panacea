83ae7256d211b0e57b06329dd2991bb1f01d1cab
grzeimann <grzeimann@gmail.com>
2018-12-02 10:57:00 -0600

    yup

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -1340,7 +1340,7 @@ allflatspec, allspec, allra, alldec, allx, ally, allsub = ([], [], [], [], [],
 
 DIRNAME = get_script_path()
 
-for info in [blueinfo[0], redinfo[1]]:
+for info in [blueinfo[0], blueinfo[1], redinfo[0], redinfo[1]]:
     specinit, specname, multi, lims, amps, slims, arc_names = info
     arc_lines = Table.read(op.join(DIRNAME, 'lrs2_config/lines_%s.dat' %
                                    specname), format='ascii')
@@ -1411,7 +1411,7 @@ for info in [blueinfo[0], redinfo[1]]:
                  (ifuslot, amp))
         bigW = get_bigW(amp, wave, trace, masterbias)
         package.append([wave, trace, bigW, masterbias, amppos, dead])
-    sys.exit(1)
+    continue
     # Normalize the two amps and correct the flat
     calinfo = [np.vstack([package[0][i], package[1][i]])
                for i in np.arange(len(package[0]))]
