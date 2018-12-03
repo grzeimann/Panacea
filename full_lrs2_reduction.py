bd0bafcf654982c12810a49f3072db16c324c826
grzeimann <grzeimann@gmail.com>
2018-12-02 19:13:54 -0600

    yup

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -1342,7 +1342,7 @@ allflatspec, allspec, allra, alldec, allx, ally, allsub = ([], [], [], [], [],
 
 DIRNAME = get_script_path()
 
-for info in [redinfo[1], redinfo[1]]:
+for info in [redinfo[0], redinfo[1]]:
     specinit, specname, multi, lims, amps, slims, arc_names = info
     arc_lines = Table.read(op.join(DIRNAME, 'lrs2_config/lines_%s.dat' %
                                    specname), format='ascii')
