5366a8214b79aeeb8e476db2fc4856453d06ca62
grzeimann <grzeimann@gmail.com>
2018-12-28 15:22:11 -0600

    first commit

--- build_standard_star_reduction_call.py
+++ build_standard_star_reduction_call.py
@@ -45,7 +45,7 @@ for f, basename in zip([standard_call], ['rstan']):
     chunks = np.array_split(f, len(f) / 20 + 1)
     for j, chunk in enumerate(chunks):
         n = len(chunk)
-        name = basename+'_%s_%i' % (args.side, j+1)
+        name = basename+'_%s_%i' % (slot, j+1)
         f = open(name+'.slurm', 'w')
         s = slurmfile.slurmstring % (n, '%j', name)
         f.write(s)
