73aa5cc97050242fc0e74b800d6ca398da9f9c97
grzeimann <grzeimann@gmail.com>
2019-01-01 09:27:48 -0600

    yup

--- build_standard_star_reduction_call.py
+++ build_standard_star_reduction_call.py
@@ -26,6 +26,10 @@ standard_call = []
 basecall = ('python /work/03730/gregz/maverick/Panacea/full_lrs2_reduction.py '
             '-d %s -s %s -ssd %s -sso %s -o %s')
 slot = sys.argv[2]
+if slot == '056':
+    names = "\"uv,orange\""
+else:
+    names = "\"red,farred\""
 for _object in object_table:
     if slot not in _object[1]:
         continue
@@ -39,7 +43,7 @@ for _object in object_table:
             obsid = op.basename(op.dirname(op.dirname(op.dirname(filename)))).split('lrs2')[1]
             date = op.basename(op.dirname(op.dirname(op.dirname(op.dirname(op.dirname(filename))))))
             standard_call.append(basecall %
-                                 (date, '"uv,orange"', date, obsid, objectname))
+                                 (date, names, date, obsid, objectname))
 
 for f, basename in zip([standard_call], ['rstan']):
     chunks = np.array_split(f, len(f) / 20 + 1)
