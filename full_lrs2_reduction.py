b0eedbafba9d7c820ad43df749d2518277e703c0
grzeimann <grzeimann@gmail.com>
2019-01-03 13:48:07 -0600

    changes

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -1050,7 +1050,7 @@ def sky_subtraction(rect, error, xloc, yloc):
         V = [np.abs(sol), 0.5 * np.median(E)]
         mult = V[np.argmin(V)] * sg
         res[:, j] = mult
-    return sky + res, model
+    return sky + res
 
 
 def make_frame(xloc, yloc, data, error, wave, dw, Dx, Dy, wstart=5700.,
@@ -1107,6 +1107,7 @@ def write_cube(wave, xgrid, ygrid, zgrid, outname, he):
 def find_source(dimage, derror, dx, dy, skysub, commonwave):
     D = np.sqrt((dx - dx[:, np.newaxis])**2 + (dy - dy[:, np.newaxis])**2)
     sn = dimage * 0.
+    print(dimage, derror)
     for i in np.arange(len(x)):
         sel = D[i, :] < 3.
         S = np.sum(dimage[sel])
@@ -1507,7 +1508,7 @@ def big_reduction(obj, bf, instrument, sci_obs, calinfo, amps, commonwave,
         e /= mini[0][3]
         if args.correct_ftf:
             r, e = correct_ftf(r, e)
-        sky, ftf_cor = sky_subtraction(r, e, pos[:, 0], pos[:, 1])
+        sky = sky_subtraction(r, e, pos[:, 0], pos[:, 1])
         sky[calinfo[-3][:, 1] == 1.] = 0.
         skysub = r - sky
         if response is not None:
@@ -1533,7 +1534,7 @@ def big_reduction(obj, bf, instrument, sci_obs, calinfo, amps, commonwave,
         if args.source_x is None:
             wi = np.searchsorted(commonwave, wave_0-wb, side='left')
             we = np.searchsorted(commonwave, wave_0+wb, side='right')
-            dimage = np.median(skysub[:, wi:we+1], axis=(1,))
+            dimage = np.median(skysub[:, wi:we+1], axis=1)
             derror = np.sqrt(np.sum(e[:, wi:we+1]**2, axis=1))*1.253 / np.sqrt(we-wi+1)
             loc1 = find_source(dimage, derror, calinfo[5][:, 0], calinfo[5][:, 1],
                                skysub, commonwave)
