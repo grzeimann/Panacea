6dca1f19acf471e03415dd0e539416ae091e68fa
grzeimann <grzeimann@gmail.com>
2019-01-10 07:34:43 -0600

    test

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -1832,6 +1832,7 @@ for info in listinfo:
         lamp_path, newdate = get_cal_path(lamp_path, args.date)
         if newdate != args.date:
             log.info('Found lamp files on %s and using them for %s' % (newdate, args.date))
+        print(lamp_path)
         masterarc = get_masterarc(lamp_path, amp, arc_names, masterbias,
                                   specname)
         fits.PrimaryHDU(masterarc).writeto('wtf_%s_%s.fits' % (ifuslot, amp), overwrite=True)
