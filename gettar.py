9f0c9a211807e91cca16a1a73f8077336eda2e21
grzeimann <grzeimann@gmail.com>
2019-05-13 09:13:43 -0500

    first

---
+++ gettar.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 13 09:06:24 2019
+
+@author: gregz
+"""
+
+import os.path as op
+import glob
+import tarfile
+from astropy.table import Table
+
+tarfolder = op.join('/work/03946/hetdex/maverick/201*', 'virus', 
+                    "{:s}00000*.tar".format('virus'))
+tarfolders = glob.glob(tarfolder)
+filenames = []
+kind = []
+for tarfolder in tarfolders:
+    print(tarfolder)
+    T = tarfile.open(tarfolder, 'r')
+    names = T.getnames()
+    flag = False
+    for name in names:
+        if name[-5:] == '.fits':
+            filenames.append(tarfolder)
+            kind.append(name[-8:-5])
+            break
+t = Table([filenames, kind])
+t.write('alltar.txt', format='ascii')
