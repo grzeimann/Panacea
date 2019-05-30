ae15800c38db40e895030e8c53274a98b6e9e31c
grzeimann <grzeimann@gmail.com>
2019-05-30 13:46:49 -0500

    testing

--- toy.py
+++ toy.py
@@ -6,20 +6,22 @@ Created on Thu May 30 13:16:47 2019
 @author: gregz
 """
 
-import sys
-import numpy as np
 import astropy.units as u
-
+import numpy as np
+import sys
+from input_utils import setup_logging
 from astropy.io import fits
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
 from hetdex_api.extract import Extract 
 from hetdex_api.survey import Survey
 
-
+log = setup_logging('toy')
+log.info('Loading Survey')
 survey = Survey('hdr1')
 t = Table(survey.hdfile.root.Survey[:])
 
+log.info('Loading External File')
 filename = '/work/03730/gregz/maverick/MUSE_WIDE_sources_for_hetdex.fits'
 
 fitsfile = fits.open(filename)
@@ -29,6 +31,8 @@ ID = bintable['ID']
 
 coords = SkyCoord(bintable['RA']*u.deg, bintable['DEC']*u.deg)
 max_sep = 11.0 * u.arcminute
+
+log.info('Finding shots of interest')
 
 matched_sources = {}
 shots_of_interest = []
@@ -40,4 +44,5 @@ for i, coord in enumerate(survey.coords):
     matched_sources[name] = idx
     if len(idx) > 0:
         shots_of_interest.append(name)
+log.info('Done.')
 print(shots_of_interest)
