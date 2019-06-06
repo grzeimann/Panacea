6de566faf138922d859f973569841bd2035cecd8
grzeimann <grzeimann@gmail.com>
2019-06-06 11:42:18 -0500

    counts

--- check_wavelength_reduction.py
+++ check_wavelength_reduction.py
@@ -78,7 +78,7 @@ for f in fn:
             peak.append(0.)
     peak = np.array(peak)
     peak /= np.max(peak)
-    print(peak)
+    print(len(peak), len(arc_lines['col3']))
     Z = np.array((len(peak), 2))
     Z[:, 0] = np.array(arc_lines['col3'])
     Z[:, 1] = peak
