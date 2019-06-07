c354d2a58e9edab9d7f7a0a5904997d87ac75829
grzeimann <grzeimann@gmail.com>
2019-06-07 10:33:04 -0500

    testing

--- full_lrs2_reduction.py
+++ full_lrs2_reduction.py
@@ -857,7 +857,7 @@ def find_lines(spectrum, trace, nlines, thresh, fib, side=None):
         EE = MR * np.sqrt(1./ph[fib]**2 + 1./ph[fib][ind])
         EE = np.max([EE, .1 * MR, 0.001 * np.ones(MR.shape)], axis=0)
         dist = v/2. + np.abs(ER - MR) / EE
-        n = np.argmin(dist)
+        n = np.argmin(v)
         print(guess, v[n], (ER - MR)[n], EE[n], ER, MR[n])
         if np.min(dist) < 10.:
             ind1 = np.argmin(dist)
