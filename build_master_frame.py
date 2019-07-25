ff882b8b5fea857bf1d571f6ec39f123c373a368
grzeimann <grzeimann@gmail.com>
2019-07-25 10:02:46 -0500

    first try

--- build_master_frame.py
+++ build_master_frame.py
@@ -116,13 +116,14 @@ def build_master_frame(file_list, ifuslot, amp, args, date):
 
     a, b = masterbias.shape
     hdu = fits.PrimaryHDU(np.array(masterbias, dtype='float32'))
-    mkpath(op.join(args.folder, date))
+    
     d1 = datetime(int(bia_list[0][2][:4]), int(bia_list[0][2][4:6]),
                   int(bia_list[0][2][6:]))
     d2 = datetime(int(bia_list[-1][2][:4]), int(bia_list[-1][2][4:6]),
                   int(bia_list[-1][2][6:]))
     d3 = d1 + timedelta(days=(d2-d1).days/2)
     avgdate = '%04d%02d%02d' % (d3.year, d3.month, d3.day)
+    mkpath(op.join(args.folder, avgdate))
     args.log.info('Writing %s_%s_%s.fits' % (mname, bia_list[-1][1], amp))
     hdu.header['OBJECT'] = '%s-%s' % (bia_list[0][2], bia_list[-1][2])
     write_fits(hdu, op.join(args.folder, avgdate, '%s_%s_%s.fits' %
