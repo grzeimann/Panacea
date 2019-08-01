ddc3d66d9d0d5bd8e76f136b961cb3c990f67355
grzeimann <grzeimann@gmail.com>
2019-08-01 10:17:19 -0500

    catch nans and use median when nans

--- math_utils.py
+++ math_utils.py
@@ -73,6 +73,8 @@ def biweight(a, nan_treatment=True, c=9.0, axis=None, calc_std=False):
     u1 = (1 - u**2)**2
     BL = (a_median + sumfunc(d * u1 * mask, axis=axis) /
           sumfunc(u1 * mask, axis))
+    badpix = np.isnan(BL)
+    BL[badpix] = a_median[badpix]
     if calc_std:
          u = u ** 2
          n = sumfunc(mask, axis=axis)
