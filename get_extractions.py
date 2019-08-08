fa80d9d25845459510ee1e763023019f0c005145
grzeimann <grzeimann@gmail.com>
2019-08-08 12:09:44 -0500

    first commit

---
+++ get_extractions.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 13:16:47 2019
+
+@author: gregz
+"""
+
+import argparse as ap
+import astropy.units as u
+import numpy as np
+import time
+
+from input_utils import setup_logging
+from astropy.io import fits
+from astropy.coordinates import SkyCoord
+from astropy.table import Table
+from astropy.table import vstack
+from datetime import datetime as dt
+from hetdex_api.extract import Extract 
+from hetdex_api.survey import Survey
+
+def toYearFraction(date):
+    def sinceEpoch(date): # returns seconds since epoch
+        return time.mktime(date.timetuple())
+    s = sinceEpoch
+
+    year = date.year
+    startOfThisYear = dt(year=year, month=1, day=1)
+    startOfNextYear = dt(year=year+1, month=1, day=1)
+
+    yearElapsed = s(date) - s(startOfThisYear)
+    yearDuration = s(startOfNextYear) - s(startOfThisYear)
+    fraction = yearElapsed/yearDuration
+
+    return date.year + fraction
+
+parser = ap.ArgumentParser(add_help=True)
+
+parser.add_argument("filename",
+                    help='''Name of fits file with Bin Table''',
+                    type=str)
+
+parser.add_argument("outputname",
+                    help='''Name of fits file output''',
+                    type=str)
+
+args = parser.parse_args(args=None)
+
+log = setup_logging('toy')
+log.info('Loading Survey')
+survey = Survey('hdr1')
+t = Table(survey.hdfile.root.Survey[:])
+
+log.info('Loading External File')
+
+fitsfile = fits.open(args.filename)
+bintable = fitsfile[1].data
+table = Table(bintable)
+
+ID = bintable['source_id']
+
+coords = SkyCoord(bintable['ra']*u.deg, bintable['dec']*u.deg)
+
+max_sep = 11.0 * u.arcminute
+
+log.info('Finding shots of interest')
+
+matched_sources = {}
+shots_of_interest = []
+E = Extract()
+# Build aperture PSF for aperture extraction
+fixed_aperture = 3.
+
+# Using box size of 10.5 (length of box side) and pixel scale of 0.25
+# To see documentation use: help(E.tophat_psf)
+aperture = E.tophat_psf(fixed_aperture, 10.5, 0.25)
+
+Sources, Spectra, Error, Weights = ([], [], [], [])
+
+for i, coord in enumerate(survey.coords):
+    dist = coords.separation(coord)
+    sep_constraint = dist < max_sep
+    name = '%sv%03d' % (t['date'][i], t['obsid'][i])
+    idx = np.where(sep_constraint)[0]
+    matched_sources[name] = idx
+    if len(idx) > 0:
+        shots_of_interest.append([coord, t['date'][i]])
+log.info('Number of shots of interest: %i' % len(shots_of_interest))
+
+for i, _info in enumerate(shots_of_interest):
+    coord = _info[0]
+    date = _info[1]
+    epoch = toYearFraction(dt(int(date[:4]), int(date[4:6]), int(date[6:8])))
+    try:
+        deltaRA = ((epoch - 2015.5) * bintable['pmra'] / 1e3 / 3600. /
+                   np.cos(bintable['dec'] * np.pi / 180.))
+        deltaDE = (epoch - 2015.5) * bintable['pmdec'] / 1e3 / 3600.
+        ncoords = SkyCoord((bintable['ra']+deltaRA)*u.deg,
+                           (bintable['dec']+deltaDE)*u.deg)
+    except:
+        log.warning("[%s] Can't convert proper motion for epoch")
+        ncoords = coords
+    dist = ncoords.separation(coord)
+    sep_constraint = dist < max_sep
+    name = '%sv%03d' % (t['date'][i], t['obsid'][i])
+    idx = np.where(sep_constraint)[0]
+    matched_sources[name] = idx
+    if len(idx) > 0:
+        log.info('Working on shot: %s' % name)
+        E.load_shot(name)
+        for ind in idx:
+            info_result = E.get_fiberinfo_for_coord(ncoords[ind], radius=7.)
+            if info_result is not None:
+                log.info('Extracting %i' % ID[ind])
+                ifux, ifuy, xc, yc, ra, dec, data, error, mask = info_result
+                weights = E.build_weights(xc, yc, ifux, ifuy, aperture)
+                result = E.get_spectrum(data, error, mask, weights)
+                spectrum_aper, spectrum_aper_error = [res for res in result]
+                Sources.append(table[ind])
+                Spectra.append(spectrum_aper)
+                Error.append(spectrum_aper_error)
+                Weights.append(weights.sum(axis=0))
+        E.fibers.close()
+
+F1 = fits.BinTableHDU(vstack(Sources))
+F1.header['EXTNAME'] = 'catalog'
+F2 = fits.ImageHDU(np.array(Spectra))
+F2.header['EXTNAME'] = 'spectra'
+F2.header['CRVAL2'] = 3470.
+F2.header['CDELTA2'] = 2.
+F3 = fits.ImageHDU(np.array(Error))
+F3.header['EXTNAME'] = 'error'
+F3.header['CRVAL2'] = 3470.
+F3.header['CDELTA2'] = 2.
+F4 = fits.ImageHDU(np.array(Weights))
+F4.header['EXTNAME'] = 'weight'
+F4.header['CRVAL2'] = 3470.
+F4.header['CDELTA2'] = 2.
+F = fits.HDUList([fits.PrimaryHDU(), F1, F2, F3, F4])
+F.writeto(args.outputname, overwrite=True)
