17de6b3b8f7ae54f3cb34839e495a672d397230a
grzeimann <grzeimann@gmail.com>
2019-08-08 11:20:10 -0500

    first commit

---
+++ build_extraction_script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  8 10:48:33 2019
+
+@author: gregz
+"""
+
+import argparse as ap
+import numpy as np
+
+from astropy.io import fits
+
+parser = ap.ArgumentParser(add_help=True)
+
+parser.add_argument("filename",
+                    help='''Name of fits file with Bin Table''',
+                    type=str)
+
+parser.add_argument("chunks",
+                    help='''Break file into this number of chunks''',
+                    type=int)
+
+args = parser.parse_args(args=None)
+
+
+fitsfile = fits.open(args.filename)
+bintable = fitsfile[1].data
+
+inds = np.argsort(bintable['ra'])
+
+s = []
+ind_chunks = np.array_split(inds, args.chunks)
+for i, ind in enumerate(ind_chunks):
+    List = [fits.PrimaryHDU(), fits.BinTableHDU(bintable[ind])]
+    F = fits.HDUList(List)
+    F.writeto('data_chunk_%03d.fits' % (i + 1))
+    s.append('python get_extractions.py data_chunk_%03d.fits' % (i + 1))
+f = open('extraction_script', 'w')
+f.write('/n'.join(s))
