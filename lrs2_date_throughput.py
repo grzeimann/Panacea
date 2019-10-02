d792ea0e0592046ecedf6a30765a0fe3c475922c
grzeimann <grzeimann@gmail.com>
2019-10-02 14:06:20 -0500

    first commit

--- lrs2_date_throughput.py
+++ lrs2_date_throughput.py
@@ -70,7 +70,7 @@ for name in ['BD+40_4032', 'BD_+17_4708', 'FEIGE_110', 'FEIGE_34',
             dT.append(D)
             d = np.interp(g[0].data[0], wave, flam)
             s.append(biweight(g[0].data[1] / d) / illum)
-    plt.plot_date(dT, np.array(s), alpha=0.6, ms=5)
+    plt.plot_date(dT, np.array(s), alpha=0.6, ms=10, marker='*')
 plt.ylim([0, 1.2])
 plt.xlim([datetime.date(2018, 10, 1), datetime.date(2019, 10, 1)])
 plt.gcf().autofmt_xdate()
