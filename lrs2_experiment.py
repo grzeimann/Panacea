620efa208c028a12d8f942d8a0eefecd2cc40ef4
grzeimann <grzeimann@gmail.com>
2020-01-31 14:37:27 -0600

    update

--- lrs2_experiment.py
+++ lrs2_experiment.py
@@ -341,7 +341,12 @@ def get_extraction_model(skysub_rect, sky_rect, def_wave, nchunks=15,
         mult = biweight(blank_image / avg[:, np.newaxis], axis=0)
         clean_chunk = clean_chunk - mult[np.newaxis, :] * avg[:, np.newaxis]
         spectra_chunk = extract_columns(model, clean_chunk)
-        schunk = schunk + res + bl[np.newaxis, :] + mult[np.newaxis, :] * avg[:, np.newaxis]
+        model_chunk = model[:, np.newaxis] * spectra_chunk[np.newaxis, :]
+        goodpca = np.isfinite(clean_chunk).sum(axis=1) > 0.75 * chunk.shape[1]
+        res = get_residual_map(clean_chunk-model_chunk, pca, goodpca)
+        clean_chunk = clean_chunk - res
+        spectra_chunk = extract_columns(model, clean_chunk)
+        schunk = schunk + mult[np.newaxis, :] * avg[:, np.newaxis] + res
         skysub_chunks.append(clean_chunk)
         sky_chunks.append(schunk)
         spec_chunks.append(spectra_chunk)
