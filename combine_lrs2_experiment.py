6f0ed321e0d2d0ff17028c3dfcd5cf9fc0953a11
grzeimann <grzeimann@gmail.com>
2020-05-29 13:33:22 -0500

    update

--- combine_lrs2_experiment.py
+++ combine_lrs2_experiment.py
@@ -152,7 +152,7 @@ for spec in [allspec, bluespec, redspec, blueerr, rederr, bluesky, redsky, c]:
     spec[spec==0.] = np.nan
 
 Blue = np.nanmean(bluespec, axis=0)
-fits.Primary(Blue).writeto('test.fits', overwrite=True)
+fits.PrimaryHDU(Blue).writeto('test.fits', overwrite=True)
 norm = np.nanmedian(bluespec / Blue[np.newaxis, :], axis=1)
 bluespec = bluespec / norm[:, np.newaxis]
 blueerr = blueerr / norm[:, np.newaxis]
