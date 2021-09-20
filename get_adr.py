1f84ab12e2cb4203e40c9491e13f11b0aa42e976
grzeimann <grzeimann@gmail.com>
2021-09-20 11:56:48 -0500

    update

--- get_adr.py
+++ get_adr.py
@@ -60,8 +60,8 @@ for filename in filenames:
         WC[j:j+N] = wc
         j += N
     
-    XC[k, WC<4650.] += 0.18
-    YC[k, WC<4650.] -= 0.35
+    XC[k, WC<4650.] += 0.20
+    YC[k, WC<4650.] -= 0.25
     sel = np.isfinite(XC[k]) * np.isfinite(YC[k])
     p0 = np.polyfit(WC[sel], XC[k][sel], 3)
     p1 = np.polyfit(WC[sel], YC[k][sel], 3)
